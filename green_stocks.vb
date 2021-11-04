a0cca2a41ab86382eb078b0fa3a7f85756fcbe2d
racruz25 <91695473+racruz25@users.noreply.github.com>
2021-11-04 17:49:23 -0500

    Module 2.2.4

--- green_stocks.vb
+++ green_stocks.vb
@@ -19,24 +19,46 @@ Sub DQAnalysis()
     Cells(3, 1).Value = "Year"
     Cells(3, 2).Value = "Total Daily Volume"
     Cells(3, 3).Value = "Return"
+    Worksheets("2018").Activate
 
-    rowStart = 2
-    rowEnd = 3013
+    'set initial volume to zero
     totalVolume = 0
 
-    Worksheets("2018").Activate
+    Dim startingPrice As Double
+    Dim endingPrice As Double
+
+    'Establish the number of rows to loop over
+    rowStart = 2
+    rowEnd = Cells(Rows.Count, "A").End(xlUp).Row
+
+    'loop over all the rows
     For i = rowStart To rowEnd
-    
-    'increase totalVolume if ticker is "DQ"
+
         If Cells(i, 1).Value = "DQ" Then
+
+            'increase totalVolume by the value in the current row
             totalVolume = totalVolume + Cells(i, 8).Value
+
+        End If
+
+        If Cells(i - 1, 1).Value <> "DQ" And Cells(i, 1).Value = "DQ" Then
+
+            startingPrice = Cells(i, 6).Value
+
+        End If
+
+        If Cells(i + 1, 1).Value <> "DQ" And Cells(i, 1).Value = "DQ" Then
+
+            endingPrice = Cells(i, 6).Value
+
         End If
 
     Next i
 
-    MsgBox (totalVolume)
-
     Worksheets("DQ Analysis").Activate
     Cells(4, 1).Value = 2018
     Cells(4, 2).Value = totalVolume
+    Cells(4, 3).Value = (endingPrice / startingPrice) - 1
+
+
 End Sub
