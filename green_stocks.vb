5be19651d6ef10d6792056ac136fcd92a564e0b2
racruz25 <91695473+racruz25@users.noreply.github.com>
2021-11-04 17:37:44 -0500

    Module 2.2.3

--- green_stocks.vb
+++ green_stocks.vb
@@ -11,14 +11,32 @@ Sub MacroCheck()
 End Sub
 
 Sub DQAnalysis()
-    
-    Worksheets("DQAnalysis").Activate
-    
+    Worksheets("DQ Analysis").Activate
+
     Range("A1").Value = "DAQO (Ticker: DQ)"
-    
+
     'Create a header row
     Cells(3, 1).Value = "Year"
     Cells(3, 2).Value = "Total Daily Volume"
     Cells(3, 3).Value = "Return"
+
+    rowStart = 2
+    rowEnd = 3013
+    totalVolume = 0
+
+    Worksheets("2018").Activate
+    For i = rowStart To rowEnd
     
+    'increase totalVolume if ticker is "DQ"
+        If Cells(i, 1).Value = "DQ" Then
+            totalVolume = totalVolume + Cells(i, 8).Value
+        End If
+
+    Next i
+
+    MsgBox (totalVolume)
+
+    Worksheets("DQ Analysis").Activate
+    Cells(4, 1).Value = 2018
+    Cells(4, 2).Value = totalVolume
 End Sub
