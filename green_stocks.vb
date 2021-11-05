af8ded17e4952d1e1b8acf06dc292e9b9d6537d1
racruz25 <91695473+racruz25@users.noreply.github.com>
2021-11-05 13:53:35 -0500

    Module 2.3.1

--- green_stocks.vb
+++ green_stocks.vb
@@ -62,3 +62,16 @@ Sub DQAnalysis()
 
 
 End Sub
+
+Sub AllStocksAnalysis()
+
+    Worksheets("All Stocks Analysis").Activate
+
+    Range("A1").Value = "All Stocks (2018)"
+
+    'Create a header row
+    Cells(3, 1).Value = "Ticker"
+    Cells(3, 2).Value = "Total Daily Volume"
+    Cells(3, 3).Value = "Return"
+
+End Sub
